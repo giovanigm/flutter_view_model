3e23d385c6adb4968eeb3d7dfa635b97e0554da4
Giovani Granero <ggmelone@gmail.com>
2023-11-16 19:35:57 -0300

    Initial ViewModel docs

--- lib/src/view_model.dart
+++ lib/src/view_model.dart
@@ -2,6 +2,21 @@ import 'dart:async';
 
 import 'package:flutter/foundation.dart';
 
+/// A [ViewModel] can manage the `State` of a View and send `Events` to it.
+///
+/// Every [ViewModel] requires an initial state which will be the
+/// state of the [ViewModel] before [emitState] has been called.
+///
+/// The current state of a [ViewModel] can be accessed via the [state] getter
+/// and the last event emitted can be accessed via the [lastEvent] getter.
+///
+/// ```dart
+/// class CounterViewModel extends ViewModel<int, void> {
+///   CounterViewModel() : super(initialState: 0);
+///
+///   void increment() => emitState(state + 1);
+/// }
+/// ```
 abstract class ViewModel<State, Event> {
   ViewModel({required State initialState}) {
     _state = initialState;
@@ -15,16 +30,24 @@ abstract class ViewModel<State, Event> {
 
   late final _eventController = StreamController<Event>.broadcast();
 
+  /// The current [state]
   State get state => _state;
 
+  /// The last event
   Event? get lastEvent => _event;
 
+  /// The state stream
   Stream<State> get stateStream => _stateController.stream;
 
+  /// The event stream
   Stream<Event> get eventStream => _eventController.stream;
 
+  /// Whether the [ViewModel] is closed.
+  ///
+  /// A [ViewModel] is considered closed once [close] is called.
   bool isClosed = false;
 
+  /// Emits a new [state]
   void emitState(State state) {
     try {
       if (isClosed) {
@@ -39,6 +62,7 @@ abstract class ViewModel<State, Event> {
     }
   }
 
+  /// Emits a new [event]
   void emitEvent(Event event) {
     try {
       if (isClosed) {
@@ -52,6 +76,7 @@ abstract class ViewModel<State, Event> {
     }
   }
 
+  /// Closes the [ViewModel]
   @mustCallSuper
   Future<void> close() async {
     await _stateController.close();
